d6c4ecec21d89b48031dfb037834c724f11731f5
sog10 <sog10@aber.ac.uk>
2013-11-24 17:37:47 +0000

    Update group.java

--- group.java
+++ group.java
@@ -6,15 +6,6 @@ public class group{
     System.out.println("edit the code");
    System.out.println("Hello World!");
   }
-  
-  public class count {
-    do while (i<100){
-     System.out.print(i);
-     i++;
-    }
-  
-  }
-  
-  
-  
 }
+
+
