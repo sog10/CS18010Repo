d1bf60543e1bd64eb60ed42d6069f59283ed6a1c
rkh4 <rkh4@aber.ac.uk>
2013-11-24 16:16:48 +0000

    Update group.java

--- group.java
+++ group.java
@@ -1,6 +1,20 @@
 public class group{
+  
+  int i =0;
+  
   public static void main (String args[]) {
     System.out.println("edit the code");
-    System.out.println("Hello World!");
+   System.out.println("Hello World!");
   }
+  
+  public class count {
+    do while (i<100){
+     System.out.print(i);
+     i++;
+    }
+  
+  }
+  
+  
+  
 }
