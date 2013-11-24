4d5a2f37d1be995557c635e4b0a06e780067d4f4
sog10 <sog10@aber.ac.uk>
2013-11-24 16:15:02 +0000

    Update group.java

--- group.java
+++ group.java
@@ -2,8 +2,5 @@ public class group{
   public static void main (String args[]) {
     System.out.println("edit the code");
     System.out.println("Hello World!");
-    Stuff stuff = new Stuff();
-    stuff.changeStuff("James");
-    System.out.println(stuff);
   }
 }
