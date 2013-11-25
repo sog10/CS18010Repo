6b47d97c2b6f92e1b3bfdb159f6c769d74829a5e
rkh4 <rkh4@aber.ac.uk>
2013-11-25 13:24:27 +0000

    Update group.java

--- group.java
+++ group.java
@@ -1,6 +1,6 @@
 public class group{
   public static void main (String args[]) {
-    System.out.println("edit the code");
+    System.out.println("an exmaple of edited stuff");
    System.out.println("Hello World!");
   }
 }
